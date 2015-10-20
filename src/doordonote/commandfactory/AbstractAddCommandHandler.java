a82a7e55913c684ebf475490cb6bfa3386f601c5
Yun Peng <chongyunpeng@gmail.com>
2015-10-20 22:38:47 +0800

    Added basic code for UpdateHandler

--- src/doordonote/commandfactory/AbstractAddCommandHandler.java
+++ src/doordonote/commandfactory/AbstractAddCommandHandler.java
@@ -1,9 +1,18 @@
 package doordonote.commandfactory;
 
+import java.util.Date;
+import java.util.List;
+
+import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
 
+import doordonote.command.AddCommand;
+import doordonote.command.Command;
+
 // TODO: Change name to make it more understandable
+// TODO: Make parser static
 public abstract class AbstractAddCommandHandler extends AbstractCommandHandler {
+	protected static final int NUM_OF_DATES_IN_EVENTS = 2;
 	protected Parser dateParser = null;
 	
 	public AbstractAddCommandHandler(String commandBody, Parser dateParser) {
@@ -11,6 +20,76 @@ public abstract class AbstractAddCommandHandler extends AbstractCommandHandler {
 		this.dateParser = dateParser;
 	}
 	
-	public 
+	protected List<Date> getDates(int indexOfTimePeriod) {		
+		if (indexOfTimePeriod < 0) {
+			return null;
+		}
+		String eventPeriodString = commandBody.substring(indexOfTimePeriod);
+		List<DateGroup> dateGroupList = dateParser.parse(eventPeriodString);
+		if (dateGroupList.isEmpty()) {
+			return null;
+		} else {
+			return dateGroupList.get(0).getDates();
+		}
+	}
+	
+	protected int getEventDateIndex() {
+		return commandBody.toLowerCase().lastIndexOf("from");
+	}
+	
+	protected int getDeadlineDateIndex() {
+		return commandBody.toLowerCase().lastIndexOf("by");
+	}
+	
+	protected boolean isMostLikelyEvent() {
+		int indexOfEventDate = getEventDateIndex();
+		int indexOfDeadlineDate = getDeadlineDateIndex();
+		return (indexOfEventDate > indexOfDeadlineDate);
+	}
+	
+	protected Date getEventStartDate() {
+		int eventDateIndex = getEventDateIndex();
+		List<Date> datesList = getDates(eventDateIndex);
+
+		if (isMostLikelyEvent() && datesList != null && datesList.size() < 2) {
+			return datesList.get(0);
+		} else {
+			return null;
+		}
+	}
+	
+	protected Date getEventEndDate() {
+		int eventDateIndex = getEventDateIndex();
+		List<Date> datesList = getDates(eventDateIndex);
+
+		if (isMostLikelyEvent() && datesList != null && datesList.size() < 2) {
+			return datesList.get(1);
+		} else {
+			return null;
+		}
+	}
+	
+	protected Date getDeadlineDate() {
+		int deadlineDateIndex = getDeadlineDateIndex();
+		List<Date> datesList = getDates(deadlineDateIndex);
+
+		if (!isMostLikelyEvent() && datesList != null && !datesList.isEmpty()) {
+			return datesList.get(0);
+		} else {
+			return null;
+		}
+	}
+	
+	
+	protected String getTaskDescription() {
+		if (isMostLikelyEvent() && getEventEndDate() != null) {
+			return commandBody.substring(0, getEventDateIndex());
+		} else if (getDeadlineDate() != null) {
+			return commandBody.substring(0, getDeadlineDateIndex());
+		} else {
+			return commandBody;
+		}
+	}
+	
 
 }
--- src/doordonote/commandfactory/UpdateHandler.java
+++ src/doordonote/commandfactory/UpdateHandler.java
@@ -1,10 +1,58 @@
 package doordonote.commandfactory;
 
-public class UpdateHandler extends AbstractCommandHandler {
+import java.util.Date;
 
-	public UpdateHandler(String commmandBody) {
-		super(commmandBody);
-		// TODO Auto-generated constructor stub
+import com.joestelmach.natty.Parser;
+
+import doordonote.command.Command;
+import doordonote.command.UpdateCommand;
+import doordonote.common.Util;
+
+public class UpdateHandler extends AbstractAddCommandHandler {
+	protected Parser dateParser = null;
+
+	public UpdateHandler(String commandBody, Parser dateParser) {
+		super(commandBody, dateParser);
 	}
+
+	
+	// TODO: Add in handler for commands such as "update 5"
+	@Override
+	public Command generateCommand() {
+		int indexToUpdate = getIndexToUpdate();
+		String taskDescription = getTaskDescription();
+		Date startDate = null;
+		Date endDate = null;
+		if (taskDescription == null || taskDescription.isEmpty()) {
+			return null;
+		} else if (isMostLikelyEvent()) {
+			startDate = getEventStartDate();
+			endDate = getEventEndDate();
+		}  else {
+			startDate = null;
+			endDate = getDeadlineDate();
+		}
+		return new UpdateCommand(indexToUpdate, taskDescription, startDate, endDate);
+	}
+	
+	@Override
+	protected String getTaskDescription() {
+		return Util.removeFirstWord(super.getTaskDescription());		
+	}
+	
+	protected int getIndexToUpdate() {
+		try {
+			int indexToUpdate = Integer.parseInt(Util.getFirstWord(commandBody));
+			if (indexToUpdate <= 0) {
+				// throw exception
+				return -1;
+			} else {
+				return indexToUpdate;
+			}
+		} catch (NumberFormatException e) {
+			return -1;
+		}		
+	}
+	
 
 }
