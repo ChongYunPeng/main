da819ddb921bd8c9627b9a440fe99ec694147bda
Yun Peng <chongyunpeng@gmail.com>
2015-10-20 23:55:21 +0800

    Removed unused overloaded add and delele methods from Controller

--- src/doordonote/logic/Controller.java
+++ src/doordonote/logic/Controller.java
@@ -1,29 +1,61 @@
 package doordonote.logic;
 
 import java.util.Date;
+import java.util.List;
 
-import doordonote.storage.Task;
+//import doordonote.storage.Task;
 
-public class Controller {
+public class Controller{
 
-	public Task add(String taskDescription, Date startDate, Date endDate) {
+	public String add(String taskDescription, Date startDate, Date endDate) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+//
+//	public String add(Task taskDeleted) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+	public String delete(int taskID) {
 		return null;
 		// TODO Auto-generated method stub
 		
 	}
 
-	public Task delete(int taskID) {
-		return null;
-		// TODO Auto-generated method stub
-		
-	}
+//	public Task delete(Task taskAdded) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
-	public Task delete(Task taskAdded) {
-		// TODO Auto-generated method stub
+	public String find(List<String> keywords) {
 		return null;
 	}
 
-	public void add(Task taskDeleted) {
+	public String find(String taskType) {
+		return null;
+	}
+	
+	public String finish(int taskID) {
+		return null;
+	}
+	
+	public String help() {
+		return null;
+	}
+	
+	public String help(String commandType) {
+		return null;
+	}
+	
+	public String redo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String undo() {
+		return null;
 		// TODO Auto-generated method stub
 		
 	}
