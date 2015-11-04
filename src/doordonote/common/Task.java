dac34acd1cac941d33051da5718561eadf014e8f
cyongrui <chenyr06@hotmail.com>
2015-11-04 10:10:11 +0800

    Added sorting for tasks spanning multiple days

--- src/doordonote/common/Task.java
+++ src/doordonote/common/Task.java
@@ -1,6 +1,8 @@
 package doordonote.common;
 
 import java.util.Date;
+import org.apache.commons.lang.time.DateUtils;
+import org.joda.time.DateTimeComparator;
 
 public abstract class Task implements Comparable<Task>{
 
@@ -19,33 +21,33 @@ public abstract class Task implements Comparable<Task>{
 	public String getDescription(){
 		return description;
 	}
-	
+
 	public boolean isDeleted(){
 		return isDeleted;
 	}
-	
+
 	public boolean isDone(){
 		return isDone;
 	}
-	
+
 	public void setDeleted(){
 		this.isDeleted = true;
 	}
-	
+
 	public void setNotDeleted(){
 		this.isDeleted = false;
 	}
-	
+
 	public void setDone(){
 		this.isDone = true;
 	}
-	
+
 	public void setNotDone(){
 		this.isDone = false;
 	}
 
 	public abstract Date getEndDate();
-	
+
 	public abstract Date getStartDate();
 
 	public abstract String getType();
@@ -57,6 +59,9 @@ public abstract class Task implements Comparable<Task>{
 	public int compareTo(Task task){
 		if(getEndDate() == null && task.getEndDate() == null){
 			return getDescription().compareTo(task.getDescription());
+		} else if(getEndDate() != null && getStartDate() != null && !(getStartDate().equals(getEndDate())) &&
+				task.getEndDate() != null && task.getStartDate() != null && !(task.getStartDate().equals(task.getEndDate()))){ 
+			return getStartDate().compareTo(task.getStartDate());
 		} else if(getEndDate() == null){
 			return 1;
 		} else if(task.getEndDate() == null){
